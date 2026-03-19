f0f7782a47dd0965202365162c6cf127d65c67e9
Andrew McWatters <andrew@andrewmcwatters.com>
2026-03-18 22:24:56 -0700

    Set up Grafana HTTPS for secure web traffic

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,3 +23,29 @@ sudo systemctl start grafana-server
 sudo systemctl status grafana-server
 
 sudo systemctl enable grafana-server.service
+
+# https://grafana.com/docs/grafana/latest/setup-grafana/set-up-https/#obtain-a-signed-certificate-from-letsencrypt
+sudo apt-get install snapd
+sudo snap install core; sudo snap refresh core
+
+sudo apt-get remove certbot
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+sudo certbot certonly --standalone
+
+# sudo ln -s /etc/letsencrypt/live/subdomain.mysite.com/privkey.pem /etc/grafana/grafana.key
+# sudo ln -s /etc/letsencrypt/live/subdomain.mysite.com/fullchain.pem /etc/grafana/grafana.crt
+
+# sudo chgrp -R grafana /etc/letsencrypt/*
+# sudo chmod -R g+rx /etc/letsencrypt/*
+# sudo chgrp -R grafana /etc/grafana/grafana.crt /etc/grafana/grafana.key
+# sudo chmod 440 /etc/grafana/grafana.crt /etc/grafana/grafana.key
+
+# ls -l /etc/grafana/grafana.*
+
+sudo sed -i 's/;protocol = http/protocol = https/' /etc/grafana/grafana.ini
+sudo sed -i 's|;cert_file =|cert_file = /etc/grafana/grafana.crt|' /etc/grafana/grafana.ini
+sudo sed -i 's|;cert_key =|cert_key = /etc/grafana/grafana.key|' /etc/grafana/grafana.ini
+
+sudo systemctl restart grafana-server
